dcf8e7f29c046164689051a7e4d9365be160225f
Christoph Keller <gri@not-censored.com>
2014-08-08 14:02:33 +0200

    Run prepare.sh before building an image

--- create-images.sh
+++ create-images.sh
@@ -2,6 +2,12 @@
 
 for dir in *; do
 	if [ -e "$dir/Dockerfile" ]; then
+		# Prepare if needed
+		if [ -e "$dir/prepare.sh" ]; then
+			$dir/prepare.sh
+		fi
+
+		# Actually build the image
 		docker build -t=buildserver/$dir $dir || exit "Failed to create $dir"
 	fi
 done
