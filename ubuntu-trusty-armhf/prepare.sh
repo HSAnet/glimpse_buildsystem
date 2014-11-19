a60612b59095e92fd658e4bfe981211921772ecc
Christoph Keller <gri@not-censored.com>
2014-11-19 15:25:37 +0100

    Remove ubuntu-precise and added utopic and trusty-armhf

---
+++ ubuntu-trusty-armhf/prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+# Prepare qemu-arm-static
+if [ ! -f qemu-arm-static ]; then
+	cp /usr/bin/qemu-arm-static qemu-arm-static
+fi
+
+IMAGENAME=ubuntu-trusty-arm
+
+# Create archlinux-arm if it does not exist
+RESULT=`docker images -q $IMAGENAME | wc -l`
+if [ $RESULT -eq 0 ]; then
+        docker import http://cdimage.ubuntu.com/ubuntu-core/releases/14.04.1/release/ubuntu-core-14.04-core-armhf.tar.gz $IMAGENAME
+fi
+
