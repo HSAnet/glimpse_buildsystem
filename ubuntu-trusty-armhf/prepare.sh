3c8638ad34d490a00d0e3229df185690455739bb
Christoph Keller <github@christophkeller.cc>
2014-11-19 16:11:24 +0100

    Chan

--- ubuntu-trusty-armhf/prepare.sh
+++ ubuntu-trusty-armhf/prepare.sh
@@ -12,6 +12,6 @@ IMAGENAME=ubuntu-trusty-arm
 # Create archlinux-arm if it does not exist
 RESULT=`docker images -q $IMAGENAME | wc -l`
 if [ $RESULT -eq 0 ]; then
-        docker import http://cdimage.ubuntu.com/ubuntu-core/releases/14.04.1/release/ubuntu-core-14.04-core-armhf.tar.gz $IMAGENAME
+        docker import http://cdimage.ubuntu.com/ubuntu-core/releases/14.04.1/release/ubuntu-core-14.04.1-core-armhf.tar.gz $IMAGENAME
 fi
 
